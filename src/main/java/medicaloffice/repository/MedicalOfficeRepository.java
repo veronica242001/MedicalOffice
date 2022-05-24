25801db81832825da0742579c175835ce275de15
veronica242001 <coiculescuveronica@gmail.com>
2022-05-24 20:16:45 +0300

    added JPA Repositories and entities

---
+++ src/main/java/medicaloffice/repository/MedicalOfficeRepository.java
@@ -0,0 +1,35 @@
+package medicaloffice;
+
+
+
+import medicaloffice.domain.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface MedicalOfficeRepository extends JpaRepository<Appointment,Long> {
+
+
+      List<Appointment> findAll();
+
+//    private MedicalOfficeRepository() {
+//        Appointment appointment = Appointment.builder()
+//                .doctor(doctor)
+//                .patient(secondPatient)
+//                .appointmentStatus(AppointmentStatus.CANCELED)
+//                .build();
+//        Appointment secondAppointment = Appointment.builder()
+//                .doctor(doctor)
+//                .patient(patient)
+//                .date(Date.from(Instant.now().plus(2, ChronoUnit.DAYS)))
+//                .appointmentStatus(AppointmentStatus.SCHEDULED)
+//                .build();
+//
+//        appointmentList.add(appointment);
+//        appointmentList.add(secondAppointment);
+//    }
+
+}
