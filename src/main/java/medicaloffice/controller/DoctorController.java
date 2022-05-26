d31902e01a9b4206f7340386751bbdd591df23eb
veronica242001 <coiculescuveronica@gmail.com>
2022-05-26 15:32:36 +0300

    added integration tests for doctorController

--- src/main/java/medicaloffice/controller/DoctorController.java
+++ src/main/java/medicaloffice/controller/DoctorController.java
@@ -6,7 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 import medicaloffice.services.DoctorService;
 import medicaloffice.entity.Doctor;
 import medicaloffice.dto.DoctorOutputDTO;
+import org.apache.coyote.Response;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,35 +16,40 @@ import java.util.stream.Collectors;
 
 
 @RestController //face automat si adnotarea de Autowired
-@RequiredArgsConstructor // genereaza constructorul
 @Slf4j // pentru log uri
 public class DoctorController {
 
     private final ModelMapper modelMapper;
     private final DoctorService doctorService;
 
-    //se pune constructor pentru a se putea face uniteste
-
+    //se pune constructor pentru a se putea face UnitTeste
+    public DoctorController(ModelMapper modelMapper, DoctorService doctorService) {
+        this.modelMapper = modelMapper;
+        this.doctorService = doctorService;
+    }
 
     @GetMapping("doctors")
-    public List<DoctorOutputDTO> getDoctors() {
+    public ResponseEntity<?> getDoctors() {
         List<Doctor> doctorsWithPrivateStuff = doctorService.findAll(); // folosim model mapper
         List<DoctorOutputDTO> doctorsDTO = doctorsWithPrivateStuff.stream()
                 .map(doctor -> modelMapper.map(doctor, DoctorOutputDTO.class))
                 .collect(Collectors.toList());
 
         log.debug("hey, our first log --> current doctors={}", doctorsDTO);
-        return doctorsDTO;
+        return ResponseEntity.ok(doctorsDTO);
     }
 
     @PostMapping("doctors")
-    public void createDoctor(@RequestBody Doctor doctor){
+    public ResponseEntity<?> createDoctor(@RequestBody Doctor doctor){
         //TODO validation
-        doctorService.addDoctor(doctor);
+       Doctor added =  doctorService.addDoctor(doctor);
+
+       return added != null ? ResponseEntity.ok(added) : ResponseEntity.badRequest().body("Doctor already exists");
     }
 
     @GetMapping("doctors/{doctorName}")
-    public Doctor getDoctor(@PathVariable String doctorName) {
-        return doctorService.findByName(doctorName);
+    public ResponseEntity<?> getDoctor(@PathVariable Long doctorId) {
+
+        return ResponseEntity.ok( modelMapper.map(doctorService.findById(doctorId), DoctorOutputDTO.class));
     }
 }
--- src/test/java/medicaloffice/integration/DoctorIT.java
+++ src/test/java/medicaloffice/integration/DoctorIT.java
@@ -1,7 +1,66 @@
 package medicaloffice.integration;
 
+import medicaloffice.controller.DoctorController;
+import medicaloffice.dto.DoctorOutputDTO;
+import medicaloffice.entity.Doctor;
+import org.junit.jupiter.api.Test;
+
+import static org.apache.http.HttpStatus.SC_BAD_REQUEST;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 @SpringBootTest
 public class DoctorIT {
+
+    @Autowired
+    private DoctorController doctorController;
+
+    @Test
+    public void test_doctorsComponent(){
+
+        //list all doctors and check that there is no doctor added
+        ResponseEntity<?> getDoctorsResponse = doctorController.getDoctors();
+        assertNotNull(" ", getDoctorsResponse);
+        assertEquals("", SC_OK, getDoctorsResponse.getStatusCode().value());
+        assertTrue("", getDoctorsResponse.getBody() instanceof List);
+
+        List<DoctorOutputDTO> result = (List<DoctorOutputDTO>) getDoctorsResponse.getBody();
+        //assertTrue("", result.isEmpty());
+
+        //add new doctor
+        Doctor doctor = Doctor.builder().firstName("Ana").lastName("Ene").build();
+        ResponseEntity<?> addDoctorResponse = doctorController.createDoctor(doctor);
+        assertNotNull(" ", addDoctorResponse);
+        assertEquals("", SC_OK, addDoctorResponse.getStatusCode().value());
+        assertTrue("", addDoctorResponse.getBody() instanceof Doctor);
+
+        Doctor added = (Doctor) addDoctorResponse.getBody();
+        assertEquals("", "Ana", added.getFirstName());
+        assertEquals("", "Ene", added.getLastName());
+
+
+        //get the doctor
+        Long doctorId = added.getId();
+        ResponseEntity<?> getDoctorResponse = doctorController.getDoctor(doctorId);
+        assertEquals("", SC_OK, getDoctorResponse.getStatusCode().value());
+        assertTrue("", getDoctorResponse.getBody() instanceof DoctorOutputDTO);
+
+        DoctorOutputDTO getDoctor = (DoctorOutputDTO) getDoctorResponse.getBody();
+        assertEquals("", "Ana", getDoctor.getFirstName());
+
+
+        //add the same doctor and check that 400 is returned
+        addDoctorResponse = doctorController.createDoctor(doctor);
+        assertEquals("", SC_BAD_REQUEST, getDoctorResponse.getStatusCode().value());
+        assertEquals("", "Doctor already exists", addDoctorResponse.getBody());
+
+    }
 }
